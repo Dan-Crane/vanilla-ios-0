52c57ab467c0a29e4ded3ad80cfa2c45c057e6e9
danZhur <gre4kaDISCOUNT@gmail.com>
2022-01-09 20:27:52 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by daniilZhuravlev on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
